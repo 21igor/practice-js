e524f4ab999951e7bacf4788bb07c7ea635f157d
Igor Seleznov and Reece Jones <reece@>
2016-05-31 16:24:52 -0700

    add the fadeIn fadeOut feature

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
-$(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".initially-showing").toggle();
-    $(".initially-hidden").toggle();
+// $(document).ready(function() {
+//   $(".clickable").click(function() {
+//     $(".initially-showing").slideToggle();
+//     $(".initially-hidden").slideToggle();
+//   });
+// });
+
+$(function () {
+  $("button#fade-in").click(function () {
+    $(".initially-showing").fadeIn();
+    $(".initially-hidden").fadeOut();
+  });
+  $("button#fade-out").click(function () {
+    $(".initially-hidden").fadeIn();
+    $(".initially-showing").fadeOut();
   });
 });
